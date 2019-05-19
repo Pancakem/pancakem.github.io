56193e4f64393f78707847b1ca3290ddc8c2f356
Pancakem <pancakesdeath@protonmail.com>
2019-05-20 00:50:49 +0300

    build home page

---
+++ src/Page/Home.elm
@@ -0,0 +1,44 @@
+module Page.Home exposing (view, Model, update, Msg)
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Types exposing (..)
+
+
+-- model 
+
+type alias Model = 
+    { posts : List Content
+    }
+
+
+-- view
+
+view : Model -> { title : String, content : Html Msg}
+view model =
+    {title = "Home"
+    , content = div [] (List.map viewSnippet model.posts)
+    }
+
+viewSnippet : Content -> Html Msg
+viewSnippet content = 
+    let
+        ct = content.blog.content
+        tease = String.dropRight ((String.length ct) - 30) ct
+    in
+    div []
+        [ a [href "" ] [text content.blog.title]
+        , p [] [text tease ]
+        ]
+
+-- update 
+
+type Msg = 
+    Clicked
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        Clicked ->
+            (model, Cmd.none)
+            
