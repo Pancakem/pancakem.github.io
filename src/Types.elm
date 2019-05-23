6e52e383e11c72f158f5a8d53c2d0b7947771b93
Pancakem <pancakesdeath@protonmail.com>
2019-05-23 19:43:53 +0300

    make types opaque

--- src/Types.elm
+++ src/Types.elm
@@ -1,8 +1,17 @@
-module Types exposing (..)
-
+module Types exposing (Tag, Blog, PublishDate, Content, createTag,
+                        createPublishDate, retrieveDateString, 
+                        retrieveTagString)
 
 type Tag = Tag String
 
+createTag : String -> Tag
+createTag str = 
+    Tag str
+
+retrieveTagString : Tag -> String
+retrieveTagString (Tag tag) = 
+    tag
+    
 type alias Blog = 
     {title : String
     , content : String 
@@ -10,9 +19,16 @@ type alias Blog =
 
 type PublishDate = PublishDate String
 
+createPublishDate : String -> PublishDate
+createPublishDate str = 
+    PublishDate str
+
+retrieveDateString : PublishDate -> String
+retrieveDateString (PublishDate pd) =
+    pd
+
 type alias Content = 
     { publishedDate : PublishDate
     , blog : Blog
     , tags : List Tag
     }
-
