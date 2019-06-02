0afdeeab8f0433a9bbc181f30e24330fb17eca25
Pancakem <pancakesdeath@protonmail.com>
2019-06-02 17:21:51 +0300

    use remote data

--- src/Page/SearchResult.elm
+++ src/Page/SearchResult.elm
@@ -3,49 +3,68 @@ module Page.SearchResult exposing (Model, Msg, init, update, view)
 import Types
 import Http
 import Json.Decode exposing (list)
-import Html exposing (Html, div)
+import Html exposing (Html, div, text, a, p)
+import Html.Attributes exposing (href)
+import RemoteData
 
 
 init : String -> (Model, Cmd Msg)
 init tag = 
-    (Model []
+    (Model RemoteData.Loading
     , getAllTagRelatedBlogs tag
     )
 
 type alias Model = 
-    { posts : List Types.Content
+    { posts : Types.WebData (List Types.Content)
     }
 
 
 type Msg = 
-    LoadResults (Result Http.Error (List Types.Content))
+    LoadResults (Types.WebData (List Types.Content))
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         LoadResults res ->
-            let
-                newModel = 
-                    case res of
-                        Ok conts ->
-                            {model | posts = conts}
-                        
-                        Err _ ->
-                            model
-            in
-            ( newModel, Cmd.none )
+            ( {model | posts = res}, Cmd.none )
 
         
 getAllTagRelatedBlogs : String -> Cmd Msg
 getAllTagRelatedBlogs str = 
     Http.get
     { url = ""
-    , expect = Http.expectJson LoadResults (list Types.contentDecoder)
+    , expect = Http.expectJson (RemoteData.fromResult >> LoadResults) (list Types.contentDecoder)
     } 
 
 view : Model -> { title: String, content : Html Msg }
 view model = 
     { title = "Results"
-    , content = div [] []
+    , content = viewWebData model
     }
+
+viewWebData : Model -> Html Msg
+viewWebData {posts} = 
+    case posts of
+        RemoteData.Loading ->
+            text "Loading..."
+
+        RemoteData.NotAsked ->
+            text ""
+
+        RemoteData.Failure err ->
+            text ("Error searching: " ++ Debug.toString err)
+        
+        RemoteData.Success data ->
+            div [] (List.map viewSnippet data)
+
+viewSnippet : Types.Content -> Html Msg
+viewSnippet content = 
+    let
+        ct = content.blog.content
+        tease = String.dropRight ((String.length ct) - 30) ct
+    in
+    div []
+        [ a [href "" ] [text content.blog.title]
+        , p [] [text tease ]
+        ]
